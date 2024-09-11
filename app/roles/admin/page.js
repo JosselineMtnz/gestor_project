9a61ea605af9a4e2a632e030db5dbafd021a960d
Esmeralda Martinez <josseline.martinez@movistar.com.sv>
2024-09-11 16:33:00 -0600

    feat: se agrego footer

--- app/roles/admin/page.js
+++ app/roles/admin/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/app/ui/navbar/pages";
+import Footer from "@/app/ui/footer/pages";
 
 function AdminPage() {
   return (
     <div>
       <Navbar></Navbar>
       <h1>Bienvenido Admin</h1>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ app/ui/footer/pages.js
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 Universidad Don Bosco. Project+. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
